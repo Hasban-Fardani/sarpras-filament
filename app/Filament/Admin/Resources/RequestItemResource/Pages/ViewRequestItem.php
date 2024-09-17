d8e389e50a93317639f51be006e8e6681fec75e4
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-09-17 15:17:18 +0700

    feat(relation|form|table): create resource, fix table column, change form

---
+++ app/Filament/Admin/Resources/RequestItemResource/Pages/ViewRequestItem.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\RequestItemResource\Pages;
+
+use App\Filament\Admin\Resources\RequestItemResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewRequestItem extends ViewRecord
+{
+    protected static string $resource = RequestItemResource::class;
+}
--- app/Models/RequestItem.php
+++ app/Models/RequestItem.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class RequestItem extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
+
+    public function details()
+    {
+        return $this->hasMany(RequestItemDetail::class);
+    }
 }
