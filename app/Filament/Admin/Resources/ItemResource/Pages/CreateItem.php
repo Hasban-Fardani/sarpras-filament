fc8cdfd8832a738c98c99cc685abcef317f79f2a
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-09-17 09:19:46 +0700

    ref(action): edit create action

--- app/Filament/Admin/Resources/ItemResource/Pages/CreateItem.php
+++ app/Filament/Admin/Resources/ItemResource/Pages/CreateItem.php
@@ -9,4 +9,11 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateItem extends CreateRecord
 {
     protected static string $resource = ItemResource::class;
+
+    protected static bool $canCreateAnother = false;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
